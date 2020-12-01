1c20eaa351fff1f34c021c2aeb8ec62993bad5ba
valekll <lordvalekllofvalour@gmail.com>
2020-11-30 21:29:25 -0500

    Firebase login page updated
    login page does not work yet, but most of it has been implemented.

--- app/src/main/java/edu/uga/cs/notuber/LoginActivity.java
+++ app/src/main/java/edu/uga/cs/notuber/LoginActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,6 +20,11 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth myAuth;
     private FirebaseUser currUser;
 
+    private EditText usernameBox;
+    private EditText passwordBox;
+    private Button loginButton;
+    private TextView signUpLinkText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,25 +32,54 @@ public class LoginActivity extends AppCompatActivity {
 
         //init Firebase auth
         myAuth = FirebaseAuth.getInstance();
+
+        //init ui elements
+        usernameBox = (EditText)findViewById(R.id.userNameBox);
+        passwordBox = (EditText)findViewById(R.id.passwordBox);
+
+        loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoginPressed(view);
+            } //onClick()
+        });
+
+        signUpLinkText = (TextView)findViewById(R.id.signUpLinkText);
+        signUpLinkText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(view.getContext(), SignUpActivity.class);
+                //auto populate sign up intent with user input values
+                signUpIntent.putExtra("USERNAME", usernameBox.getText().toString());
+                signUpIntent.putExtra("PASSWORD", passwordBox.getText().toString());
+                startActivity(signUpIntent);
+            } //onClick()
+        });
     } //onCreate()
 
+    /**
+     * Logs in the app if a user already is logged in.
+     */
     @Override
     protected void onStart() {
         super.onStart();
         currUser = myAuth.getCurrentUser();
-        if(currUser == null) {
-            currUser = getNewUser();
+        if(currUser != null) {
+            loginToLanding(currUser);
         } //if
     } //onStart()
 
     /**
-     * Gets a user to login into the app.
-     * @return the new user
+     * Tries to log in user.
+     * @param view the button view
      */
-    private FirebaseUser getNewUser() {
-        FirebaseUser newUser = null;
-        return newUser;
-    } //getNewUser()
+    private void onLoginPressed(View view) {
+        String username = usernameBox.getText().toString();
+        String password = passwordBox.getText().toString();
+        if(username != null && password != null) {
+        } //if
+    } //onLoginPressed
 
     /**
      * Logins into the app and sends user into landing activity.
