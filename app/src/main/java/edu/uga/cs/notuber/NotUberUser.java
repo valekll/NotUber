5f6af2d2e192f918ff5d7bd82b5d927ad82c1e49
valekll <lordvalekllofvalour@gmail.com>
2020-12-06 19:48:38 -0500

    User class modified
    Getters and setters added

--- app/src/main/java/edu/uga/cs/notuber/NotUberUser.java
+++ app/src/main/java/edu/uga/cs/notuber/NotUberUser.java
@@ -8,17 +8,50 @@ public class NotUberUser {
     private String email;
     private String password;
 
+    //constructor
     public NotUberUser() {
     }
 
+    //constructor
     public NotUberUser(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
+    //constructor
     public NotUberUser(String username, String email, String password) {
         this.username = username;
         this.email = email;
+        this.password = password;
+    }
+
+    //getter for username
+    public String getUsername() {
+        return username;
+    }
+
+    //setter for username
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    //getter for email address
+    public String getEmail() {
+        return email;
+    }
+
+    //setter for email address
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //getter for password
+    public String getPassword() {
+        return password;
+    }
+
+    //setter for password
+    public void setPassword(String password) {
         this.password = password;
     }
 } //NotUberUser
