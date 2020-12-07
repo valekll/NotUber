76e2e7ed6425ab4fcda6de10c1be4017f8b87dfc
valekll <lordvalekllofvalour@gmail.com>
2020-12-07 18:50:14 -0500

    Basic logout button added

--- app/src/main/java/edu/uga/cs/notuber/LandingMainActivity.java
+++ app/src/main/java/edu/uga/cs/notuber/LandingMainActivity.java
@@ -1,9 +1,14 @@
 package edu.uga.cs.notuber;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -12,6 +17,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LandingMainActivity extends AppCompatActivity {
 
     private static final String DEBUG_TAG = "Destinations";
+
+    private FirebaseAuth myAuth;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,19 @@ public class LandingMainActivity extends AppCompatActivity {
         // /res/layout/activity_country_main.xml will be used;
         // in landscape orientation /res/layout-land/activity_country_main.xml will be used
         setContentView( R.layout.activity_landing_main);
-    }
+
+        myAuth = FirebaseAuth.getInstance();
+        logoutButton = (Button)findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myAuth.signOut();
+                Intent loginIntent = new Intent(LandingMainActivity.this,
+                        LoginActivity.class);
+                startActivity(loginIntent);
+            } //onClick()
+        });
+    } //onCreate()
 
 
-}
+} //LandingActivityMain
