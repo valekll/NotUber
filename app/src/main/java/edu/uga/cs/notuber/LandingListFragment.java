28e29bd4fc68086f6c6ae4f8ad6ff15572e90aa2
valekll <lordvalekllofvalour@gmail.com>
2020-12-08 18:02:16 -0500

    code cleaned up

--- app/src/main/java/edu/uga/cs/notuber/LandingListFragment.java
+++ app/src/main/java/edu/uga/cs/notuber/LandingListFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,9 +25,6 @@ public class LandingListFragment extends ListFragment {
 
     // Array of destination strings for the list fragment
     public static List<RideListing> rideListings;
-
-    // indicate if this is a landscape mode with both fragments present or not
-    boolean twoFragmentsActivity = false;
 
     // list selection index
     int listingIndex = 0;
@@ -50,13 +49,7 @@ public class LandingListFragment extends ListFragment {
             rideListings.add(ride);
         } //for
         // create a new ArrayAdapter for the list
-        setListAdapter( new ArrayAdapter<>( getActivity(), android.R.layout.simple_list_item_activated_1, rideListings ) );
-
-        //set the twoFragmentsActivity variable to true iff we are in 2 fragment (landscape) view
-        View detailsFrame = getActivity().findViewById( R.id.destinationInfo );
-
-        twoFragmentsActivity = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
-
+        setListAdapter( new ArrayAdapter<>( getActivity(), R.layout.list_item_for_array_adapter, rideListings ) );
 
         // restore the saved list index selection (listing index), if available
         if(savedInstanceState != null) {
@@ -67,25 +60,28 @@ public class LandingListFragment extends ListFragment {
 
         // set the list mode as single choice and
         getListView().setChoiceMode( ListView.CHOICE_MODE_SINGLE );
-        getListView().setItemChecked( listingIndex, false );
+        getListView().setItemChecked( listingIndex, true );
 
-        // if we are in 2 fragment (landscape) orientation, show the Android version information on the right side
-        // This class will act as the "driver" here by displaying the details using the other fragment.
-        if( twoFragmentsActivity ) {
+        // The list in the landscape orientation may be shorter and the selected item
+        // which was visible in portrait mode might be invisible (out of view)
+        // i.e., "below" the end of the screen in landscape orientation.
+        // To make it visible again, call smoothScrollToPosition
+        getListView().smoothScrollToPosition( listingIndex );
+        /*
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("Titanium", "onclick activated");
+                showListingInfo(i);
+            } //onItemClick()
+        });
 
-            // display the information about the selected destination in the fragment on the right (details)
-            showListingInfo( listingIndex );
-
-            // The list in the landscape orientation may be shorter and the selected item
-            // which was visible in portrait mode might be invisible (out of view)
-            // i.e., "below" the end of the screen in landscape orientation.
-            // To make it visible again, call smoothScrollToPosition
-            getListView().smoothScrollToPosition( listingIndex );
-        } //if
+         */
     } //onActivityCreated()
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
+        Log.d("Titanium", "onlistitemclick activated");
         showListingInfo(position);
     } //onListItemClick()
 
@@ -95,48 +91,20 @@ public class LandingListFragment extends ListFragment {
 
         // save the list index selection
         outState.putInt( LandingFragment.LISTINGINDEX, listingIndex);
-
     } //onSaveInstanceState()
 
-    void showListingInfo( int listingIndex ) {
+    private void showListingInfo( int listingIndex ) {
 
         this.listingIndex = listingIndex;
 
-        if(twoFragmentsActivity) {  // only in the 2 fragment (landscape) orientation
-
-            getListView().setItemChecked( listingIndex, false );
-
-            Log.d("TETRIS", "line 110 will be executed");
-            // get the placeholder element (FrameLayout) in a 2 fragment (landscape) orientation layout
-            LandingFragment details = (LandingFragment) getFragmentManager().findFragmentById(R.id.destinationInfo);
-
-            if( details == null || details.getShownListingIndex() != listingIndex ) {
-
-                Log.d("TETRIS", "line 116 will be executed");
-                // obtain a new destination info fragment instance
-                details = LandingFragment.newInstance(rideListings.get(listingIndex).getRideId());
-
-                // replace the placeholder with the new fragment stance within a transaction
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace( R.id.destinationInfo, details );
-
-                // TRANSIT_FRAGMENT_FADE means that the fragment should fade in or fade out
-                fragmentTransaction.setTransition( FragmentTransaction.TRANSIT_FRAGMENT_FADE );
-
-                // commit the transaction, i.e. make the changes
-                fragmentTransaction.commit();
-            } //if
-        } //if
-        else {
-            Log.d("TETRIS", "line 132 will be executed");
-            // In a 1 fragment orientation (portrait), start a new activity using an Intent, as in the previous demo app
-            Intent intent = new Intent();
-            intent.setClass( getActivity(), LandingInfoActivity.class );
-            intent.putExtra(LandingFragment.RIDEIDTAG, rideListings.get(listingIndex).getRideId());
-            intent.putExtra(LandingFragment.LISTINGINDEX, listingIndex);
-
-            startActivity( intent );
-        } //else
+        Log.d("Titanium", "listingIndex: " + listingIndex);
+        Log.d("TETRIS", "line 132 will be executed");
+        // In a 1 fragment orientation (portrait), start a new activity using an Intent, as in the previous demo app
+        Intent intent = new Intent(getContext(), LandingInfoActivity.class);
+        Log.d("TETRIS", "intent created");
+        intent.putExtra(LandingFragment.RIDEIDTAG, rideListings.get(listingIndex).getRideId());
+        intent.putExtra(LandingFragment.LISTINGINDEX, listingIndex);
+        startActivity(intent);
     } //showListingInfo()
 
     @Override
