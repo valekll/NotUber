a11668ebd7df74cdbd78853c9d9a807a32e7f14a
HR47779 <55899689+HR47779@users.noreply.github.com>
2020-12-08 14:58:37 -0500

    Update LandingFragment.java

--- app/src/main/java/edu/uga/cs/notuber/LandingFragment.java
+++ app/src/main/java/edu/uga/cs/notuber/LandingFragment.java
@@ -2,13 +2,10 @@ package edu.uga.cs.notuber;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,7 +15,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 /**
