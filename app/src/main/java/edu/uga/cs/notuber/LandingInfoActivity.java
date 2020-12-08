618841b0bc9be8420a7b020209c95143402814b2
valekll <lordvalekllofvalour@gmail.com>
2020-12-08 00:39:29 -0500

    Actionbar Added to LandingInfoActivity

--- app/src/main/java/edu/uga/cs/notuber/LandingInfoActivity.java
+++ app/src/main/java/edu/uga/cs/notuber/LandingInfoActivity.java
@@ -1,10 +1,13 @@
 package edu.uga.cs.notuber;
 
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -25,20 +28,10 @@ public class LandingInfoActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
-        // IMPORTANT:
-        // Add the back button in the ActionBar of this activity.  It will appear
-        // as a left chevron (arrow tip), the regular back button.
-        //
-        // We can't use the Parent specification in the AndroidManifest, since
-        // it would place the activity on the back stack.  Consequently, when going
-        // back to the screen with the list of Destinations (using the "parent-specified"
-        // back button, this would cause a recreation of the ListFragment.  The new list
-        // would show the initial item (Destination) as selected, not the one we
-        // actually selected when transitioning to the Destination details screen.
-        //
-        // However, there must be a listener added for this back button (look below).
-        //ActionBar actionBar = getSupportActionBar();
-        //actionBar.setDisplayHomeAsUpEnabled( false );
+        // Add the back button in the ActionBar of this activity.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        actionBar.setDisplayHomeAsUpEnabled( true );
 
         // if this call is in landscape orientation, do nothing and return,
         // as the main activity w/ ListFragment will do the work.
@@ -74,11 +67,8 @@ public class LandingInfoActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Implement Back button listener method.
         // This method may be used for other actions from the ActionBar menu, if provided in the layout.
-        int id = item.getItemId();
-
         // android.R.id.home is the built-in designation of the back button widget.
-
-        if( id == android.R.id.home ) {
+        if(item.getItemId() == android.R.id.home ) {
             onBackPressed();
             return true;
         }
