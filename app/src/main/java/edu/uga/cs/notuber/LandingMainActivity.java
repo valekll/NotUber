a70e182901dc0e1401227384e6e36c580cc752a4
HR47779 <55899689+HR47779@users.noreply.github.com>
2020-12-08 02:35:15 -0500

    Updated a lot of shit

--- app/src/main/java/edu/uga/cs/notuber/LandingMainActivity.java
+++ app/src/main/java/edu/uga/cs/notuber/LandingMainActivity.java
@@ -1,16 +1,18 @@
 package edu.uga.cs.notuber;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.widget.Toolbar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
@@ -26,21 +28,41 @@ public class LandingMainActivity extends AppCompatActivity implements Navigation
 
     private FirebaseAuth myAuth;
     private Button logoutButton;
+    private ClipData.Item logout;
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
 
+    private MenuItem logoutItem;
+    private Menu navMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_main);
+        myAuth = FirebaseAuth.getInstance();
         toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.nav_view);
+        navMenu = navigationView.getMenu();
+
+        logoutItem = navMenu.findItem(R.id.logout);
+
+        logoutItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick (MenuItem menuItem) {
+                    myAuth.signOut();
+                    Intent loginIntent = new Intent(LandingMainActivity.this,
+                            LoginActivity.class);
+                    startActivity(loginIntent);
+                return true;
+            }
+        });
+
+
 
         drawerLayout = findViewById(R.id.drawer_Layout);
-        navigationView = findViewById(R.id.nav_view);
-
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
                 this,
                 drawerLayout,
@@ -60,24 +82,26 @@ public class LandingMainActivity extends AppCompatActivity implements Navigation
         // /res/layout/activity_country_main.xml will be used;
         // in landscape orientation /res/layout-land/activity_country_main.xml will be used
 
-
-        myAuth = FirebaseAuth.getInstance();
-        logoutButton = (Button)findViewById(R.id.logoutButton);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                myAuth.signOut();
-                Intent loginIntent = new Intent(LandingMainActivity.this,
-                        LoginActivity.class);
-                startActivity(loginIntent);
-            } //onClick()
-        });
     } //onCreate()
+
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
 
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        switch (item.getItemId()) {
+
+            case R.id.logout:
+                // Handle logout click
+                Toast.makeText(LandingMainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 
     @Override
